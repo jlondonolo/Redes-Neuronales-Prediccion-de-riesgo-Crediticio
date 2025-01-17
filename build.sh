75bdd3787a280c374261bb440b2ea4ecb5d8b2ed
José Daniel Londoño Londoño <jlondonolo@unal.edu.co>
2025-01-17 09:27:42 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 # exit on error
 set -o errexit
 
+# Crear directorio faltante y establecer permisos
+mkdir -p /var/lib/apt/lists/partial
+chmod -R 644 /var/lib/apt/lists/partial
+
 # Instalar dependencias del sistema
 apt-get update
 apt-get install -y \
