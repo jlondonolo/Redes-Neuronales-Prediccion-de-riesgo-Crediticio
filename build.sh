901c5de61475676d35a5d116bf5a57b1eaf75ea5
José Daniel Londoño Londoño <jlondonolo@unal.edu.co>
2025-01-17 09:29:10 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,18 +2,6 @@
 # exit on error
 set -o errexit
 
-# Crear directorio faltante y establecer permisos
-mkdir -p /var/lib/apt/lists/partial
-chmod -R 644 /var/lib/apt/lists/partial
-
-# Instalar dependencias del sistema
-apt-get update
-apt-get install -y \
-    build-essential \
-    python3-dev \
-    gfortran \
-    libopenblas-dev
-
 # Instalar dependencias de Python
 pip install --upgrade pip
 pip install -r requirements.txt
