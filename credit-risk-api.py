b5142dd03dbbb29c3e1552cc2f7824e4a88fb6ed
José Daniel Londoño Londoño <jlondonolo@unal.edu.co>
2025-01-29 15:11:48 -0500

    Update credit-risk-api.py

--- credit-risk-api.py
+++ credit-risk-api.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+from fastapi import FastAPI, Request
 import pandas as pd
 import numpy as np
 import joblib
